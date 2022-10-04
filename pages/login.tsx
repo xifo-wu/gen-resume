f8514b4e0aa41f727f430b0e17aba3611178d9ab
Xifo <xifo.wu@gmail.com>
2022-10-04 16:28:24 +0800

    feat: use new login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,182 +1,150 @@
-import { ReactElement, useMemo, useState } from 'react';
-import {
-  Box,
-  IconButton,
-  Container,
-  TextField,
-  Paper,
-  Typography,
-  Button,
-  Divider,
-  Grid,
-} from '@mui/material';
-import LoadingButton from '@mui/lab/LoadingButton';
+import { ReactElement, useRef, useState } from 'react';
 import router from 'next/router';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { Theme, lighten, Grid } from '@mui/material';
+import { useCookies } from 'react-cookie';
 import { toast } from 'react-toastify';
-import { useForm, Controller } from 'react-hook-form';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import GoogleIcon from '@mui/icons-material/Google';
-import FullBackgroundArea from '@/components/FullBackgroundArea';
+import { useForm } from 'react-hook-form';
 import Link from '@/Link';
+import { apiPost } from '@/api';
 import SimpleAppBarLayout from '@/layouts/SimpleAppBarLayout';
-import styles from '@/styles/loginPageStyles';
-import api from '@/api';
+import Container from '@/components/SignUpPage/Container';
+import ContentContainer from '@/components/SignUpPage/ContentContainer';
+import Form from '@/components/Form';
+import InputField from '@/components/Form/InputField';
+import SubTitle from '@/components/SignUpPage/SubTitle';
+import Title from '@/components/SignUpPage/Title';
+import PasswordField from '@/components/Form/PasswordField';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface FormData {
-  email: string;
+  login: string;
   password: string;
 }
 
-const backgroundUrl =
-  'https://images.unsplash.com/photo-1648315156503-5335899e3470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
+const contentBoxSx = (theme: Theme) => ({
+  [theme.breakpoints.down('md')]: {
+    borderRadius: 1,
+    background: '#fff',
+    boxShadow: '0 0 35px rgba(0, 0, 0, 0.1)',
+    margin: 'auto',
+    maxWidth: 640,
+  },
+});
 
-const Login = () => {
+const SignUp = () => {
+  const [submitting, setSubmitting] = useState(false);
+
   const {
     control,
+    reset,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm<FormData>({
     defaultValues: {
-      email: '',
+      login: '',
       password: '',
     },
   });
-  const [loading, setLoading] = useState(false);
 
-  const toastConfig = useMemo(
-    () => ({
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    }),
-    [],
-  );
+  const password = useRef({});
+  password.current = watch('password', '');
 
   const onSubmit = async (payload: FormData) => {
-    setLoading(true);
-    const { data } = await api<any>({
-      method: 'POST',
-      url: '/api/v1/user/login',
+    setSubmitting(true);
+    const { status, data, error } = await apiPost<FormData, any>({
+      url: '/api/v1/auth/login/using-password',
       data: payload,
     });
-    setLoading(false);
 
-    if (!data.success) {
-      toast.error(data.message, toastConfig);
+    if (status >= 400 && error) {
+      setSubmitting(false);
+      toast.error(error.message);
       return;
     }
 
-    window.localStorage.setItem('accessToken', data.meta.accessToken);
+    if (data) {
+      toast.success('欢迎回来 🎉 ');
+    }
+
+    reset();
+    await new Promise((r) => setTimeout(r, 1000));
+    const { meta } = data;
+    window.localStorage.setItem('accessToken', meta.token);
+    setSubmitting(false);
     router.push('/dashboard');
   };
 
   return (
-    <>
-      <FullBackgroundArea src={backgroundUrl} sx={{ zIndex: -1, position: 'fixed' }} />
-      <Container>
-        <Grid container justifyContent="center" alignItems="center" sx={{ minHeight: '100vh' }}>
-          <Grid item xs={12} sm={8} md={6}>
-            <Paper elevation={0} sx={styles.loginPaper}>
-              <Typography sx={styles.subTitle}>Login</Typography>
-              <Typography variant="h4" sx={styles.title}>
-                登录
-              </Typography>
-              <Typography variant="subtitle2" sx={{ mt: 2 }}>
-                还没有账号吗？那就
-                <Link href="/sign-up" sx={{ textDecoration: 'none' }}>
-                  &nbsp;创建一个新账号&nbsp;
-                </Link>
-                吧！
-              </Typography>
+    <Container>
+      <Grid container>
+        <Grid item xl={5} lg={5} md={6} xs={12} sx={contentBoxSx}>
+          <ContentContainer>
+            <Title>欢迎回来</Title>
+            <SubTitle>
+              还没有账号？
+              <Link href="/sign-up" sx={{ textDecoration: 'none' }}>
+                &nbsp;注册&nbsp;
+              </Link>
+            </SubTitle>
+            <Form onSubmit={handleSubmit(onSubmit)} sx={{ mt: 4 }}>
+              <InputField<FormData>
+                name="login"
+                control={control}
+                errors={errors}
+                rules={{
+                  required: '用户名不能为空',
+                }}
+                inputField={{
+                  label: '用户名',
+                  placeholder: '使用用户名或邮箱登录'
+                }}
+              />
 
-              <Box sx={styles.contentBox}>
-                <Box
-                  component="form"
-                  sx={{
-                    px: 1,
-                    '& .MuiTextField-root': { my: 2 },
-                  }}
-                  autoComplete="off"
-                  onSubmit={handleSubmit(onSubmit)}
-                >
-                  <Controller
-                    name="email"
-                    control={control}
-                    rules={{
-                      required: '邮箱不能为空',
-                      pattern: {
-                        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                        message: '邮箱格式不正确',
-                      },
-                    }}
-                    render={({ field }) => (
-                      <TextField
-                        label="邮箱"
-                        variant="outlined"
-                        sx={{ width: '100%' }}
-                        error={!!errors.email}
-                        helperText={errors.email?.message}
-                        {...field}
-                      />
-                    )}
-                  />
+              <PasswordField<FormData>
+                name="password"
+                control={control}
+                errors={errors}
+                rules={{
+                  required: '密码不能为空',
+                }}
+                inputField={{
+                  label: '密码',
+                }}
+              />
 
-                  <Controller
-                    name="password"
-                    control={control}
-                    rules={{ required: '密码不能为空' }}
-                    render={({ field }) => (
-                      <TextField
-                        label="密码"
-                        type="password"
-                        variant="outlined"
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                        sx={{ width: '100%' }}
-                        {...field}
-                      />
-                    )}
-                  />
-
-                  <LoadingButton
-                    loading={loading}
-                    size="large"
-                    variant="contained"
-                    type="submit"
-                    fullWidth
-                    sx={{ my: 1 }}
-                  >
-                    登录
-                  </LoadingButton>
-                </Box>
-
-                <Box sx={{ mt: 3 }}>
-                  <Divider>第三方账号登录</Divider>
-                  <Box sx={{ display: 'flex', gap: 2, mt: 1, justifyContent: 'center' }}>
-                    <IconButton disabled>
-                      <GitHubIcon />
-                    </IconButton>
-
-                    <IconButton disabled>
-                      <GoogleIcon />
-                    </IconButton>
-                  </Box>
-                </Box>
-              </Box>
-            </Paper>
-          </Grid>
+              <LoadingButton
+                loading={submitting}
+                // fix MUI warning
+                startIcon={<></>}
+                loadingPosition="start"
+                size="large"
+                variant="contained"
+                type="submit"
+                fullWidth
+                sx={(theme) => ({
+                  my: 2,
+                  height: 56,
+                  fontSize: '1.125rem',
+                  background: `linear-gradient(126deg, ${theme.palette.primary.main}, ${lighten(
+                    theme.palette.primary.main,
+                    0.45,
+                  )});`,
+                })}
+              >
+                {submitting ? '登录中，请稍后' : '登录'}
+              </LoadingButton>
+            </Form>
+          </ContentContainer>
         </Grid>
-      </Container>
-    </>
+      </Grid>
+    </Container>
   );
 };
 
-Login.getLayout = function getLayout(page: ReactElement) {
-  return <SimpleAppBarLayout>{page}</SimpleAppBarLayout>;
+SignUp.getLayout = function getLayout(page: ReactElement) {
+  return <SimpleAppBarLayout titleColor="#fff">{page}</SimpleAppBarLayout>;
 };
 
-export default Login;
+export default SignUp;
